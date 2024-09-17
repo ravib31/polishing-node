03f85809589e640b634da09b1eae351fc1672ef4
ravib31 <rkb0808@gmail.com>
2024-09-17 13:34:04 +0530

    L3 done

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var name = "Pranam node baba"
 var age = 20;
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
+// console.log(global)/*its a global object provide by node js */
+console.log(globalThis) /*its a global object provide by node js,but global is not working in the browser but globalThis will work in the browser */
+
+console.log(global===globalThis) /*true */
+
+console.log(this)/*this will throw a empty object in the vs code but it will open the global object in the browser */
+ 
