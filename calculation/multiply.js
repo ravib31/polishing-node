4eae817d815460414e7879a2aafe5661ab64260b
ravib31 <rkb0808@gmail.com>
2024-09-18 22:44:02 +0530

    lec 5

--- calculation/multiply.js
+++ calculation/multiply.js
@@ -1,4 +1,16 @@
-function mul(a,b){
-    console.log(a*b);
+// function mul(a,b){
+//     console.log(a*b);
+// }
+// module.exports = mul;
+
+// (function(){
+//     var a = 20;
+//     console.log(a)
+// })();
+function add(a,b){
+    var a = 20;
+    var b = 30;
+    console.log(a+b);
 }
-module.exports = mul;
+add()
+var a = 20;
