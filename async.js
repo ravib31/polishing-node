1044c77155d9583db63bf786bc4aaecc456efb2b
ravib31 <rkb0808@gmail.com>
2024-09-22 22:58:58 +0530

    Lec 7 finished

--- async.js
+++ async.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
+const crypto = require('crypto');/* crypto is a built in mudule of node js console is also a module in node js*/
 const https = require('https');
 
 console.log('start');
 
+crypto.pbkdf2Sync('password', 'salt', 100000, 64, 'sha512');
+console.log('First key is generate');
+
 fs.readFileSync('File2.txt','utf-8');
+crypto.pbkdf2('password', 'salt', 10000000, 64, 'sha512',(err,key)=>{
+    console.log(key.toString('hex'));
+});
+
+// 100000 is no of  ittration
+// sha512 is algorithm
+// 64 key length in bytes
 
 https.get('https://fakestoreapi.com/products/1',(res)=>{
     console.log(res.statusCode);
