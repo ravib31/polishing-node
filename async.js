8970c8b51ffbfb19988218db49eb1f2e32725b3b
ravib31 <rkb0808@gmail.com>
2024-09-22 21:52:03 +0530

    synchronous function block the main thread

--- async.js
+++ async.js
@@ -3,15 +3,17 @@ const https = require('https');
 
 console.log('start');
 
+fs.readFileSync('File2.txt','utf-8');
+
 https.get('https://fakestoreapi.com/products/1',(res)=>{
     console.log(res.statusCode);
 });
 
 
 setTimeout(()=>{
-    console.log('setTimeout 0');
+    console.log('setTimeout after 5 sec');
 },5000);
 
-fs.readFile('text.txt','utf-8',(err,data)=>{
+fs.readFile('File.txt','utf-8',(err,data)=>{
     console.log('File Data :',data);
 });
