381e624f0c974eee91785317ef72d289f823249d
ravib31 <rkb0808@gmail.com>
2024-10-06 01:46:08 +0530

    mongodb is a connector like mongoose but we can not use this for deployment

--- databse.js
+++ databse.js
@@ -3,21 +3,38 @@ const { MongoClient } = require('mongodb');
 // or as an es module:
 // import { MongoClient } from 'mongodb'
 
+
 // Connection URL
-const url = 'mongodb+srv://ravibhashkar0102:HZd6yMO6hY9RTOcv@cluster0.t7vfq.mongodb.net/namste-node'
+const url = 'mongodb+srv://ravibhashkar0102:HZd6yMO6hY9RTOcv@cluster0.t7vfq.mongodb.net/'
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'Namste-Node';
+const dbName = 'namste-node';
 
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
-  const collection = db.collection('User');
+  const collection = db.collection('user');
 
   // the following code examples can be pasted here...
+
+
+
+// const data = {
+//     "name":"sam",
+//     "location":"orisha",
+//     "occupation":"Developer"
+// }
+
+// const insertResult = await collection.insertMany([data]);
+// console.log('Inserted documents =>', insertResult);
+
+
+
+  const findResult = await collection.find({}).toArray();
+console.log('Found documents =>', findResult);
 
   return 'Connection stablished.';
 }
